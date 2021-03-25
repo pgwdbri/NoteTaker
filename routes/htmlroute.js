12a31307711d8870cd5bc97d45f1013a4c9064e2
Bri <bri.krystalle@gmail.com>
2021-03-24 21:59:49 -0400

    routes

---
+++ routes/htmlroute.js
@@ -0,0 +1,18 @@
+var router = require("express").Router();
+var PATH = require("path")
+
+
+router.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
+router.get("/notes", function(req,res){
+    res.sendFile(path.join(__dirname,"../public/notes.html"))
+})
+
+
+router.get("*", function(req,res){
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
+module.exports = routes;
