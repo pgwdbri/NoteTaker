0476f5d139ff5f73929a73dba22b485e78a2559b
Bri <bri.krystalle@gmail.com>
2021-03-24 21:53:29 -0400

    setting up server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+let express = require("express");
+let PORT = 3000;
+let app = express()
+app.use(express.urlencoded{
+    extended:true
+})
+app.use(express.json())
+app.use(express.static("public"))
+
+var apiroute = require("./routes/apiroute")
+var htmlroute = require("./routes/htmlroute")
+app.use(apiroute)
+app.use(htmlroute)
+
+app.listen(PORT, function(){
+    console.log("App running on port",PORT)
+})
